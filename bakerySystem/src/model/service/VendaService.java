db29baa9f7e5100984054b8164549711661f4906
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-05-10 13:11:33 -0300

    add da pacote e classes service

--- bakerySystem/src/model/service/VendaService.java
+++ bakerySystem/src/model/service/VendaService.java
@@ -2,12 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model.bean;
+package model.service;
+
+import model.bean.Caixa;
+import model.bean.Pessoa;
+import model.bean.Venda;
+
 /**
  *
  * @author jonat
  */
-public class GerenciaVenda {
+public class VendaService {
     
     public boolean realizaVenda(Pessoa pessoa, Venda venda, Caixa caixa) {
         return true;
