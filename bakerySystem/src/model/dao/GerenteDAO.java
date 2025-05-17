610415093fb783f4eff1454817a108abf368cb84
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-05-17 14:13:48 -0300

    Update GerenteDAO.java
    Co-Authored-By: José Otávio Ávila Alves Pires <184294446+Saigo0@users.noreply.github.com>

--- bakerySystem/src/model/dao/GerenteDAO.java
+++ bakerySystem/src/model/dao/GerenteDAO.java
@@ -4,10 +4,178 @@
  */
 package model.dao;
 
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import model.bean.Produto;
+
 /**
  *
  * @author jonat
  */
 public class GerenteDAO {
-    
+
+    private static final String SQL_INSERT_GERENTE = "INSERT INTO gerente (nome, preco, tipo) VALUES (?, ?, ?)";
+    private static final String SQL_SELECT_ALL_GERENTE = "SELECT id_gerente, nome, CPF, telefone, cargo, login, senha FROM gerente";
+    private static final String SQL_UPDATE_GERENTE = "UPDATE caixa SET nome = ?, CPF = ?, telefone = ?, cargo = ?, login = ?, senha = ? WHERE gerente.id_gerente = ?";
+    private static final String SQL_DELETE_GERENTE = "DELETE FROM gerente WHERE gerente.id_gerente = ?";
+    private static final String SQL_FIND_BY_ID = "SELECT * FROM gerente WHERE gerente.id_gerente = ?";
+
+    public Produto create(Produto produto) {
+        PreparedStatement stmt = null;
+        Connection connection = ConnectionFactory.getConnection();
+        ResultSet generatedKeys = null;
+
+        try {
+            stmt = connection.prepareStatement(SQL_INSERT_PRODUTO, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setString(3, produto.getTipo());
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                generatedKeys = stmt.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    produto.setIdProduto(generatedKeys.getLong(1));
+                    JOptionPane.showMessageDialog(null, "Produto criado com sucesso! ID: " + produto.getIdProduto());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Falha ao obter o ID!");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Falha ao criar o produto! Nenhuma linha foi afetada!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar o Produto!" + ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, stmt, generatedKeys);
+        }
+        return produto;
+    }
+
+    public List<Produto> read() {
+        List<Produto> produtos = new ArrayList<>();
+
+        PreparedStatement stmt = null;
+        Connection connection = ConnectionFactory.getConnection();
+        ResultSet rs = null;
+
+        try {
+            stmt = connection.prepareStatement(SQL_SELECT_ALL_PRODUTOS);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Produto produto = new Produto();
+
+                produto.setIdProduto(rs.getLong("id_produto"));
+                produto.setNome(rs.getString("nome"));
+                produto.setPreco(rs.getDouble("preco"));
+                produto.setTipo(rs.getString("tipo"));
+
+                produtos.add(produto);
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao tentar realizar a query dos produtos!" + ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, stmt, rs);
+        }
+        return produtos;
+    }
+
+    public boolean update(Produto produto) {
+        PreparedStatement stmt = null;
+        Connection connection = ConnectionFactory.getConnection();
+
+        if (produto == null || produto.getIdProduto() == null || produto.getIdProduto() <= 0L) {
+            throw new IllegalArgumentException("Produto para atualização é nulo ou não tem um ID válido!");
+        }
+
+        try {
+            stmt = connection.prepareStatement(SQL_UPDATE_PRODUTO);
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setString(3, produto.getTipo());
+            stmt.setLong(4, produto.getIdProduto());
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                JOptionPane.showMessageDialog(null, "Produto atualizado com sucesso!");
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Nenhum produto foi encontrado ou os dados são os mesmos!");
+                return false;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar o Produto!" + ex.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection(connection, stmt);
+        }
+        return false;
+    }
+
+    public boolean delete(Long id) {
+        PreparedStatement stmt = null;
+        Connection connection = ConnectionFactory.getConnection();
+
+        if (id <= 0) {
+            throw new IllegalArgumentException("O id precisa ser maior que 0");
+//            JOptionPane.showMessageDialog(null, "O ID precisa ser maior que 0!");
+        }
+
+        try {
+            stmt = connection.prepareStatement(SQL_DELETE_PRODUTO);
+            stmt.setLong(1, id);
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                JOptionPane.showMessageDialog(null, "Produto excluido com sucesso!");
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Nenhum produto com o ID correspondente foi encontrado!");
+                return false;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir o produto!" + ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, stmt);
+        }
+        return false;
+    }
+
+    public Produto findById(Long idProduto) {
+
+        PreparedStatement stmt = null;
+        Connection connection = ConnectionFactory.getConnection();
+        ResultSet rs = null;
+        Produto produto = new Produto();
+
+        try {
+            stmt = connection.prepareStatement(SQL_FIND_BY_ID);
+            stmt.setLong(1, idProduto);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                produto.setIdProduto(rs.getLong("id_produto"));
+                produto.setNome(rs.getString("nome"));
+                produto.setPreco(rs.getDouble("preco"));
+                produto.setTipo(rs.getString("tipo"));
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao tentar realizar a query dos produtos!" + ex.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection(connection, stmt, rs);
+        }
+        return produto;
+    }
+
 }
